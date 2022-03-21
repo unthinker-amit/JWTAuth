d4eeb3f18052fa4051e923b28b7110911b98d01e
unthinker-amit <amit.kumar@unthinkable.co>
2022-03-21 15:02:47 +0530

    JWT token

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from .models import StudentModel
 from .serializers import StudentSerializer
 from rest_framework.viewsets import ModelViewSet
 
+
 # Create your views here.
 class StudentViewSet(ModelViewSet):
     queryset=StudentModel.objects.all()
--- jwtAuth/urls.py
+++ jwtAuth/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path,include
 from api import views
 from rest_framework.routers import DefaultRouter
-
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
 router=DefaultRouter()
 router.register('api',views.StudentViewSet,basename="viewSet")
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('gettoken/',TokenObtainPairView.as_view()),
+    path('refreshtoken/',TokenRefreshView.as_view()),
+    path('verifytoken/',TokenVerifyView.as_view()),
     path("",include(router.urls)),
 ]
