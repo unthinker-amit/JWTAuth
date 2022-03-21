bd14eaef758df34e52cabc1cf61fe8d7ebcb5817
unthinker-amit <amit.kumar@unthinkable.co>
2022-03-21 12:35:41 +0530

    View set

---
+++ api/views.py
@@ -0,0 +1,11 @@
+from .models import StudentModel
+from .serializers import StudentSerializer
+from rest_framework.viewsets import ModelViewSet
+
+# Create your views here.
+class StudentViewSet(ModelViewSet):
+    queryset=StudentModel.objects.all()
+    serializer_class=StudentSerializer      
+
+
+
